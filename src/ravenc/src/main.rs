c2fa14786275cb0024ba09ced3f51e5a8a23132a
Marshall Bowers <elliott.codes@gmail.com>
2021-10-29 23:38:15 -0400

    Add basic IPC from `ravenc` to `ravenwm`

--- src/ravenc/src/main.rs
+++ src/ravenc/src/main.rs
@@ -1,3 +1,14 @@
+use std::{io::Write, os::unix::net::UnixStream};
+
 fn main() {
     println!("Hello, world from ravenc!");
+
+    let socket_path = std::env::var("RAVENWM_SOCKET").expect("Failed to read RAVENWM_SOCKET");
+
+    let mut stream =
+        UnixStream::connect(&socket_path).expect(&format!("Failed to connect to {}", socket_path));
+
+    stream
+        .write_all(&"Hello from ravenc".as_bytes())
+        .expect("Failed to write to socket")
 }
--- src/ravenwm/src/main.rs
+++ src/ravenwm/src/main.rs
@@ -1,7 +1,14 @@
+use std::{io::Read, os::unix::net::UnixListener};
+
 use xcb;
 
 fn main() {
     println!("Hello, world from ravenwm!");
+
+    let socket_path = std::env::var("RAVENWM_SOCKET").expect("Failed to read RAVENWM_SOCKET");
+
+    let listener =
+        UnixListener::bind(&socket_path).expect(&format!("Failed to connect to {}", socket_path));
 
     let (conn, preferred_screen) = xcb::Connection::connect(Some(":1")).unwrap();
     let setup = conn.get_setup();
@@ -50,6 +57,22 @@ fn main() {
     conn.flush();
 
     loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    let mut message = String::new();
+                    stream
+                        .read_to_string(&mut message)
+                        .expect("Faield to read message");
+
+                    println!("Message: {}", message);
+                }
+                Err(err) => {
+                    println!("{}", err);
+                }
+            }
+        }
+
         if let Some(event) = conn.wait_for_event() {
         } else {
             break;
