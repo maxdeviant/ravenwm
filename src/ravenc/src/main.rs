bd3ac249248114d22d60504e1672df2a64b20d92
Marshall Bowers <elliott.codes@gmail.com>
2021-10-30 14:41:18 -0400

    Add CLI commands to `ravenc`

--- src/ravenc/src/main.rs
+++ src/ravenc/src/main.rs
@@ -1,8 +1,31 @@
 use ravenwm_core::ipc;
+use structopt::StructOpt;
 
-fn main() {
+#[derive(StructOpt, Debug)]
+#[structopt(name = "ravenc")]
+struct Args {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(rename_all = "snake_case")]
+enum Command {
+    MoveWindow { x: u32, y: u32 },
+    CloseWindow,
+}
+
+#[paw::main]
+fn main(args: Args) {
     let socket = ipc::SocketPath::new();
     let mut ipc_client = ipc::Client::connect(&socket);
 
-    ipc_client.send(&ipc::Message::MoveWindow { x: 500, y: 500 });
+    match args.command {
+        Command::MoveWindow { x, y } => {
+            ipc_client.send(&ipc::Message::MoveWindow { x, y });
+        }
+        Command::CloseWindow => {
+            ipc_client.send(&ipc::Message::CloseWindow);
+        }
+    }
 }
